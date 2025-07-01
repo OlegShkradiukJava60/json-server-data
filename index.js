f429bfbc4a62c72000d80e7411f7be9c323858e1
Olegshkradiuk <Olegwills40@gmail.com>
2025-07-01 18:42:08 +0300

    hw48 solution

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import { faker } from '@faker-js/faker'
 const N_EMPLOYEES = 100;
 function createRandomEmployee() {
-    const res = {};
-    const gender = faker.helpers.arrayElement(["female", "male"])
-    res.id = faker.string.uuid();
-    res.department = faker.helpers.arrayElement(["QA", "Development", "Audit", "Accounting","Management"]);
-    res.fullName = faker.person.fullName({sex: gender});
-    res.avatar = getAvatar(gender);
-    res.salary = faker.number.int({min:5000, max: 49999, multipleOf:100});
-    res.birthDate = faker.date.birthdate({min: 20, max: 72, mode: "age"}).toISOString().substring(0, 10);
-    res.userId="ADMIN";
-    return res;
+  const res = {};
+  const gender = faker.helpers.arrayElement(["female", "male"])
+  res.id = faker.string.uuid();
+  res.department = faker.helpers.arrayElement(["QA", "Development", "Audit", "Accounting", "Management"]);
+  res.fullName = faker.person.fullName({ sex: gender });
+  res.avatar = getAvatar(gender);
+  res.salary = faker.number.int({ min: 5000, max: 49999, multipleOf: 100 });
+  res.birthDate = faker.date.birthdate({ min: 20, max: 72, mode: "age" }).toISOString().substring(0, 10);
+  res.userId = "ADMIN"
+  return res;
 }
 function getAvatar(gender) {
   let res = faker.image.avatar();
@@ -18,10 +18,10 @@ function getAvatar(gender) {
   return res;
 }
 function createRandomEmployees() {
-   return Array.from({length: N_EMPLOYEES}, () => createRandomEmployee());
+  return Array.from({ length: N_EMPLOYEES }, () => createRandomEmployee());
 
 }
 const jsonRes = {
-    "employees":createRandomEmployees()
+  "employees": createRandomEmployees()
 }
 console.log(JSON.stringify(jsonRes, null, 2))
